c7f38bdee8c186a46fa84cdae03b724b43527c27
Robinson Carvallo <robinson.carvallo@csmarta.cl>
2023-09-20 12:05:14 -0300

    Reconstruccion proyecto sism

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sismActual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
